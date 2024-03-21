5aa9bfd66fec165450f6623eba7c3c072f5ab82b
jaz <jazlynntroop@gmail.com>
2024-03-21 15:41:47 -0400

    test

---
+++ src/GamePanel.java
@@ -0,0 +1,14 @@
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+public class GamePanel extends JPanel implements ActionListener {
+
+    GamePanel() {
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
+
--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,6 +1,8 @@
 public class SnakeGame {
     public static void main(String[] args){
 
+        GameFrame frame = new GameFrame();
+
 
     }
 }
